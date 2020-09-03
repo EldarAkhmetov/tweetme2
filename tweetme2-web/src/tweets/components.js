6233d928e89d6cfdfc7d74827de42a6cd3a8a15e
Eldar Akhmetov <ahmetov20@yandex.ru>
2020-09-03 14:08:52 +0700

    add new tweets at frontend

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -2,32 +2,48 @@ import React, {useEffect, useState} from 'react';
 import {loadTweets} from '../lookup';
 
 export const TweetsComponent = (props) => {
+  const textAreaRef = React.createRef();
+  const [newTweets, setNewTweets] = useState([]);
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event);
+    const newVal = textAreaRef.current.value;
+    let tempNewTweets = [...newTweets];
+    tempNewTweets.unshift({
+      content: newVal,
+      likes: 0,
+      id: 34254325324,
+    });
+    setNewTweets(tempNewTweets);
+    textAreaRef.current.value = '';
   }
   
   return <div className={props.className}>
     <div className='col-12 mb-3'>
       <form onSubmit={handleSubmit}>
-        <textarea className='form-control w-25 mx-auto' name='tweet'>
+        <textarea ref={textAreaRef} required className='form-control w-25 mx-auto' name='tweet'>
 
         </textarea>
         <button type='submit' className='btn btn-primary my-3'>Tweet</button>
       </form>
     </div>
-    <TweetsList />
+    <TweetsList newTweets={newTweets} />
   </div>
 }
 
 export const TweetsList = (props) => {
+  const [tweetsInit, setTweetsInit] = useState([]);
   const [tweets, setTweets] = useState([]);
-    
+  useEffect(() => {
+    const final = [...props.newTweets].concat(tweetsInit);
+    if (final.length !== tweets.length) {
+      setTweets(final);
+    }
+  }, [props.newTweets, tweets, tweetsInit]);    
   useEffect(() => {
     //do my lookup
     const myCallback = (response, status) => {
       if (status === 200) {
-        setTweets(response);
+        setTweetsInit(response);
       } else {
         alert("There was an error")
       }
@@ -38,7 +54,7 @@ export const TweetsList = (props) => {
   }, [])
   
   return tweets.map((item, index) => {
-    return <Tweet tweet={item} key={`${index}-${item.id}`} className="my-5 py-5 border border-danger bg-white text-dark" />
+    return <Tweet tweet={item} key={`${index}-${item.id}`} className="my-5 py-5 border border-danger bg-white text-dark w-50 mx-auto" />
   });
 };
 
