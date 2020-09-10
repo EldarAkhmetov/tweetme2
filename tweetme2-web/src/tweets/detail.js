0f0cba33fc5114ac1f6a9c8b14a1ec42de7266ee
Eldar Akhmetov <ahmetov20@yandex.ru>
2020-09-10 15:41:08 +0700

    refactor

--- tweetme2-web/src/tweets/detail.js
+++ tweetme2-web/src/tweets/detail.js
@@ -2,6 +2,11 @@ import React, {useState} from 'react';
 
 import {ActionBtn} from './buttons';
 
+import {
+  UserDisplay, 
+  UserPicture
+} from '../profiles';
+
 export const ParentTweet = (props) => {
     const {tweet} = props;
     return tweet.parent ? <Tweet isRetweet retweeter={props.retweeter} hideActions className=' ' tweet={tweet.parent} /> : null;
@@ -35,20 +40,16 @@ export const ParentTweet = (props) => {
   
     return <div className={className}>
       {isRetweet === true && <div className='mb-2'>
-  <span className='small text-muted'>Retweet via @{retweeter.username}</span>
+        <span className='small text-muted'>Retweet via <UserDisplay user={retweeter} /></span>
       </div>}
       <div className='d-flex'>
         <div className='col-1'>
-          <span className='mx-1 px-3 py-2 rounded-circle bg-dark text-white'>
-            {tweet.user.username[0]}
-          </span>
+          <UserPicture user={tweet.user} />
         </div>
         <div className='col-11'>       
           <div>
             <p>
-              {tweet.user.first_name}{" "}
-              {tweet.user.last_name}{" "}
-              @{tweet.user.username}
+            <UserDisplay includeFullName user={tweet.user} />
             </p>
             <p>{tweet.content}</p>
             <ParentTweet tweet={tweet} retweeter={tweet.user}/>
